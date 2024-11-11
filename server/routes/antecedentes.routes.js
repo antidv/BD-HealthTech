7174bc8abc547def547790c3687a64ba1fdd5fc5
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-11 14:16:36 -0500

    crud antecedentes

--- server/routes/antecedentes.routes.js
+++ server/routes/antecedentes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAntecedentes, postAntecedentes, updateAntecedentes, deleteAntecedentes } from '../controllers/antecedentes.controller.js';
+import { getAntecedentes, postAntecedentes, updateAntecedentes } from '../controllers/antecedentes.controller.js';
 import { verifyToken, isPaciente } from '../controllers/auth.middleware.js';
 
 
@@ -8,6 +8,5 @@ const router = Router();
 router.get('/antecedentes', verifyToken, isPaciente, getAntecedentes);
 router.post('/antecedentes', verifyToken, isPaciente, postAntecedentes);
 router.put('/antecedentes', verifyToken, isPaciente, updateAntecedentes);
-router.delete('/antecedentes', verifyToken, isPaciente, deleteAntecedentes);
 
 export default router;
