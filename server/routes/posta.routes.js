28fba5c61830e4f4e292006d6ac286b987d117e1
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-29 17:39:28 -0500

    deleteMedicos

--- server/routes/posta.routes.js
+++ server/routes/posta.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getPostas, getPosta, postPosta, updatePosta, deletePosta } from '../controllers/posta.controller.js';
+import { getPostas, getPosta, postPosta, updatePosta } from '../controllers/posta.controller.js';
 import { verifyToken, isAdmin } from '../libs/auth.middleware.js';
 
 const router = Router();
@@ -8,6 +8,6 @@ router.get('/postas', verifyToken, getPostas);
 router.get('/postas/:id', verifyToken, getPosta);
 router.post('/postas', verifyToken, isAdmin, postPosta);
 router.put('/postas/:id', verifyToken, isAdmin, updatePosta);
-router.delete('/postas/:id', verifyToken, isAdmin, deletePosta);
+router.delete('/postas/:id', verifyToken, isAdmin);
 
 export default router;
