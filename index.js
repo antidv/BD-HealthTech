0a98b6505d26afbc3544c20be7e9363e0d6c6251
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-30 13:14:33 -0500

    organizacion de index

--- index.js
+++ index.js
@@ -1,53 +1,7 @@
-// index.js
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import app from './server/src/app.js';
 
-// Importar rutas existentes
-import antecedentesRoutes from './server/routes/antecedentes.routes.js';
-import authRoutes from './server/routes/auth.routes.js';
-import consultorioPostaRoutes from './server/routes/consultorioPosta.routes.js';
-import horarioRoutes from './server/routes/horario.routes.js';
-import medicoConsultorioPostaRoutes from './server/routes/medicoConsultorioPosta.routes.js'
-import programacionCitaRoutes from './server/routes/programacionCita.routes.js'
-import medicosRoutes from './server/routes/medicos.routes.js';
-import pacientesRoutes from './server/routes/pacientes.routes.js';
-import postaRoutes from './server/routes/posta.routes.js';
-
-
-// Importar nuevas rutas
-import citaRoutes from './server/routes/cita.routes.js';
-import diagnosticoRoutes from './server/routes/diagnostico.routes.js';
-import recetaRoutes from './server/routes/receta.routes.js';
-
-// Crear instancia de Express
-const app = express();
-
-// Middlewares
-app.use(morgan('dev'));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(antecedentesRoutes);
-app.use(authRoutes);
-app.use(consultorioPostaRoutes);
-app.use(pacientesRoutes);
-app.use(postaRoutes);
-app.use(horarioRoutes);
-app.use(medicosRoutes);
-app.use(medicoConsultorioPostaRoutes);
-app.use(programacionCitaRoutes)
-app.use(citaRoutes);
-app.use(diagnosticoRoutes);
-app.use(recetaRoutes);
-
-// Iniciar el servidor
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log('Servidor corriendo en el puerto', PORT);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,14 @@
-// src/app.js
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-// Importar rutas existentes
 import antecedentesRoutes from '../routes/antecedentes.routes.js';
 import authRoutes from '../routes/auth.routes.js';
-import conMediPostaRoutes from '../routes/conMediPosta.routes.js';
 import consultorioPostaRoutes from '../routes/consultorioPosta.routes.js';
 import horarioRoutes from '../routes/horario.routes.js';
-import medicoPostaRoutes from '../routes/medicoPosta.routes.js';
+import medicoConsultorioPostaRoutes from '../routes/medicoConsultorioPosta.routes.js'
+import programacionCitaRoutes from '../routes/programacionCita.routes.js'
 import medicosRoutes from '../routes/medicos.routes.js';
 import pacientesRoutes from '../routes/pacientes.routes.js';
 import postaRoutes from '../routes/posta.routes.js';
@@ -18,25 +16,27 @@ import citaRoutes from '../routes/cita.routes.js';
 import diagnosticoRoutes from '../routes/diagnostico.routes.js';
 import recetaRoutes from '../routes/receta.routes.js';
 
-// Middlewares
 const app = express();
+
 app.use(morgan('dev'));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
-// Usar rutas existentes
-app.use('/api', antecedentesRoutes);
-app.use('/api', authRoutes);
-app.use('/api', conMediPostaRoutes);
-app.use('/api', consultorioPostaRoutes);
-app.use('/api', horarioRoutes);
-app.use('/api', medicoPostaRoutes);
-app.use('/api', medicosRoutes);
-app.use('/api', pacientesRoutes);
-app.use('/api', postaRoutes);
-app.use('/api', citaRoutes);
-app.use('/api', diagnosticoRoutes);
-app.use('/api', recetaRoutes);
+app.use(antecedentesRoutes);
+app.use(authRoutes);
+app.use(consultorioPostaRoutes);
+app.use(pacientesRoutes);
+app.use(postaRoutes);
+app.use(horarioRoutes);
+app.use(medicosRoutes);
+app.use(medicoConsultorioPostaRoutes);
+app.use(programacionCitaRoutes)
+app.use(citaRoutes);
+app.use(diagnosticoRoutes);
+app.use(recetaRoutes);
 
 export default app;
