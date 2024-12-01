56b3e46fe714e7cc4c4f7eff616837f36aab34c8
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-12-01 01:31:38 -0500

    getMedico

--- server/controllers/medicos.controller.js
+++ server/controllers/medicos.controller.js
@@ -96,17 +96,83 @@ export const getMedicos = async (req, res) => {
 export const getMedico = async (req, res) => {
   try {
     const connection = await pool.getConnection();
-    const rows = await connection.query(
-      "SELECT * FROM medico WHERE idmedico = ?",
-      [req.params.id]
-    );
-    if (rows.length <= 0) {
+    const { id } = req.params;
+
+    const medicoQuery = `
+      SELECT 
+        m.idmedico, 
+        m.nombre, 
+        m.apellidoP, 
+        m.dni, 
+        m.especialidad, 
+        m.foto, 
+        m.disponible
+      FROM medico m
+      WHERE m.idmedico = ?
+    `;
+    const medicoRows = await connection.query(medicoQuery, [id]);
+
+    if (medicoRows.length <= 0) {
+      connection.release();
       return res.status(404).json({ error: "El médico no existe" });
     }
-    res.status(200).json(rows[0]);
-    connection.end();
+
+    const medico = medicoRows[0];
+
+    const consultorioPostaQuery = `
+      SELECT 
+        p.nombre AS nombre_posta, 
+        c.nombre AS nombre_consultorio, 
+        cp.disponible AS estado_consultorio
+      FROM medico_consultorio_posta mcp
+      JOIN consultorio_posta cp ON mcp.idconsultorio_posta = cp.idconsultorio_posta
+      JOIN posta p ON cp.idposta = p.idposta
+      JOIN consultorio c ON cp.idconsultorio = c.idconsultorio
+      WHERE mcp.idmedico = ?
+    `;
+    const consultorioPostaRows = await connection.query(consultorioPostaQuery, [id]);
+
+    connection.release();
+
+    if (!Array.isArray(consultorioPostaRows)) {
+      return res.status(500).json({ error: "Error al obtener los consultorios y postas asociados" });
+    }
+
+    const postas = consultorioPostaRows.reduce((result, row) => {
+      const posta = result.find(p => p.nombre_posta === row.nombre_posta);
+      if (!posta) {
+        result.push({
+          nombre_posta: row.nombre_posta,
+          consultorios: [
+            {
+              nombre_consultorio: row.nombre_consultorio,
+              estado: row.estado_consultorio,
+            },
+          ],
+        });
+      } else {
+        posta.consultorios.push({
+          nombre_consultorio: row.nombre_consultorio,
+          estado: row.estado_consultorio,
+        });
+      }
+      return result;
+    }, []);
+
+    const result = {
+      idmedico: medico.idmedico,
+      nombre: medico.nombre,
+      apellidoP: medico.apellidoP,
+      dni: medico.dni,
+      especialidad: medico.especialidad,
+      foto: medico.foto,
+      estado: medico.estado,
+      postas: postas
+    };
+
+    res.status(200).json(result);
   } catch (error) {
-    console.error(error);
+    console.error("Error al obtener el médico:", error);
     res.status(500).send("Error al obtener el médico");
   }
 };
