0f760208f2dc822e21502de1011b86f7aa1ea78d
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-12-01 17:37:00 -0500

    paciente foto aniadido estatico

--- client/src/components/cards/CardPaciente.jsx
+++ client/src/components/cards/CardPaciente.jsx
@@ -1,9 +1,11 @@
+import FotoPaciente from "../../assets/paciente.png"
+
 function CardMedico(props) {
     return (
       <div className="col-4 justify-content-center">
         <div className="card">
           <div className="d-flex justify-content-center">
-            <img src={props.foto} className="card-img-top imageCard" alt="paciente"  />
+            <img src={FotoPaciente} className="card-img-top imageCard" alt="paciente"  />
           </div>
           <div className="card-body">
             <h5 className="card-title">{props.nombre}</h5>
