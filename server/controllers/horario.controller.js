9c4f3210e29e4b041dd900079e877afdac921839
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-12-01 16:19:45 -0500

    horarios

--- server/controllers/horario.controller.js
+++ server/controllers/horario.controller.js
@@ -1,43 +1,29 @@
 import { pool } from "../src/database.js";
 
-export const postHorario = async (req, res) => {
-    try {
-        const { hora_inicio, hora_fin } = req.body;
-
-        const connection = await pool.getConnection();
-        try {
-            await connection.beginTransaction();
-
-            const result = await connection.query(
-                `INSERT INTO horario (hora_inicio, hora_fin)
-                 VALUES (?, ?)`,
-                [hora_inicio, hora_fin]
-            );
-
-            await connection.commit();
-            res.json({ idhorario: result.insertId.toString() });
-        } catch (error) {
-            await connection.rollback();
-            throw error;
-        } finally {
-            connection.release();
-        }
-    } catch (error) {
-        console.error("Error al crear el horario:", error);
-        res.status(500).send("Error al registrar el horario");
-    }
-};
-
 export const getHorarios = async (req, res) => {
     try {
         const connection = await pool.getConnection();
-        const rows = await connection.query('SELECT * FROM horario');
-        res.status(200).json(rows);
-        connection.end();
+        const rows = await connection.query('SELECT idhorario, hora_inicio, hora_fin FROM horario');
+        connection.release();
+
+        const formattedRows = rows.map(row => ({
+            idhorario: row.idhorario,
+            hora: `${formatTime(row.hora_inicio)} - ${formatTime(row.hora_fin)}`
+        }));
+
+        res.status(200).json(formattedRows);
     } catch (error) {
         console.error("Error al obtener los horarios:", error);
         res.status(500).send("Error al obtener los horarios");
     }
+};
+
+const formatTime = (time) => {
+    const [hours, minutes] = time.split(':');
+    const date = new Date();
+    date.setHours(hours, minutes);
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    return date.toLocaleTimeString('en-US', options);
 };
 
 export const getHorario = async (req, res) => {
@@ -52,48 +38,5 @@ export const getHorario = async (req, res) => {
     } catch (error) {
         console.error("Error al obtener el horario:", error);
         res.status(500).send("Error al obtener el horario");
-    }
-};
-
-export const updateHorario = async (req, res) => {
-    try {
-        const connection = await pool.getConnection();
-        const { id } = req.params;
-        const { hora_inicio, hora_fin } = req.body;
-
-        const result = await connection.query(
-            `UPDATE horario SET hora_inicio = ?, hora_fin = ? WHERE idhorario = ?`,
-            [hora_inicio, hora_fin, id]
-        );
-
-        if (result.affectedRows === 0) {
-            return res.status(404).json({ error: "El horario no existe" });
-        }
-
-        const updatedHorario = await connection.query('SELECT * FROM horario WHERE idhorario = ?', [id]);
-        res.json(updatedHorario[0]);
-        connection.end();
-    } catch (error) {
-        console.error("Error al actualizar el horario:", error);
-        res.status(500).send("Error al actualizar el horario");
-    }
-};
-
-export const deleteHorario = async (req, res) => {
-    try {
-        const connection = await pool.getConnection();
-        const { id } = req.params;
-
-        const result = await connection.query('DELETE FROM horario WHERE idhorario = ?', [id]);
-
-        if (result.affectedRows === 0) {
-            return res.status(404).json({ error: "El horario no existe" });
-        }
-
-        res.json({ message: "Horario eliminado con éxito" });
-        connection.end();
-    } catch (error) {
-        console.error("Error al eliminar el horario:", error);
-        res.status(500).send("Error al eliminar el horario");
     }
 };
--- server/routes/horario.routes.js
+++ server/routes/horario.routes.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import { postHorario, getHorarios, getHorario, updateHorario, deleteHorario } from '../controllers/horario.controller.js';
+import { getHorarios, getHorario } from '../controllers/horario.controller.js';
 import { verifyToken, isAdmin } from '../libs/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/horarios', verifyToken, isAdmin, postHorario);
-router.get('/horarios', verifyToken, isAdmin, getHorarios);
-router.get('/horarios/:id', verifyToken, isAdmin, getHorario);
-router.put('/horarios/:id', verifyToken, isAdmin, updateHorario);
-router.delete('/horarios/:id', verifyToken, isAdmin, deleteHorario);
+router.get('/horarios', verifyToken, getHorarios);
+router.get('/horarios/:id', verifyToken, getHorario);
 
 export default router;
