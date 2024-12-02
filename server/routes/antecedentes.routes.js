2117b22a7618a8ffd6e1a1195e8d447529799722
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-12-02 01:22:40 -0500

    antecedentes-alergias

--- server/routes/antecedentes.routes.js
+++ server/routes/antecedentes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAntecedentes, postAntecedentes, updateAntecedentes } from '../controllers/antecedentes.controller.js';
+import { getAntecedenteAleEnf, getAntecedentes, postAntecedentes, updateAntecedentes } from '../controllers/antecedentes.controller.js';
 import { verifyToken, isPaciente, isMedico } from '../libs/auth.middleware.js';
 
 
@@ -9,5 +9,6 @@ router.get('/antecedentes', verifyToken, isPaciente, getAntecedentes);
 router.get('/antecedentes', verifyToken, isMedico, getAntecedentes);
 router.post('/antecedentes', verifyToken, isMedico, postAntecedentes);
 router.put('/antecedentes', verifyToken, isMedico, updateAntecedentes);
+router.get('/antecedentes-ale-enf/:idpaciente', verifyToken, isMedico, getAntecedenteAleEnf)
 
 export default router;
