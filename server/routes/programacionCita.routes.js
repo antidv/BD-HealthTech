8c5bc8459d16fa0e777a9e679d3593787f255796
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2024-12-01 21:39:13 -0500

    programacioncita-ciudad

--- server/routes/programacionCita.routes.js
+++ server/routes/programacionCita.routes.js
@@ -3,13 +3,15 @@ import {
   getProgramacionesCita,
   getProgramacionCita,
   postProgramacionCita,
-  deleteProgramacionCita
+  deleteProgramacionCita,
+  getCitasProgramadas
 } from '../controllers/programacionCita.controller.js';
 import { verifyToken, isAdmin } from '../libs/auth.middleware.js';
 
 const router = Router();
 
 router.get('/programacion-cita', verifyToken, getProgramacionesCita);
+router.get('/programacioncita-ciudad', verifyToken, getCitasProgramadas);
 router.get('/programacion-cita/:id', verifyToken, getProgramacionCita);
 router.post('/programacion-cita', verifyToken, isAdmin, postProgramacionCita);
 router.delete('/programacion-cita/:id', verifyToken, isAdmin, deleteProgramacionCita);
