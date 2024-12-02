1827f6185e8e3dccd7e9b75106aa844ba9c66a49
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-12-02 01:17:26 -0500

    rol

--- server/routes/medicos.routes.js
+++ server/routes/medicos.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { getMedicos, getMedico, perfilMedico, postMedico, updateMedicos } from '../controllers/medicos.controller.js';
-import { verifyToken, isAdmin } from '../libs/auth.middleware.js';
+import { verifyToken, isAdmin, isMedico } from '../libs/auth.middleware.js';
 
 const router = Router();
 
 router.get('/medicos', verifyToken, isAdmin, getMedicos);
-router.get('/medicos/perfil', verifyToken, perfilMedico);
+router.get('/medicos/perfil', verifyToken, isMedico, perfilMedico);
 router.get('/medicos/:id', verifyToken, isAdmin, getMedico);
 router.post('/medicos', verifyToken, isAdmin, postMedico);
 router.put('/medicos/:id', verifyToken, isAdmin, updateMedicos);
